92d89433eaf711399da3e456fee8719337f8606e
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-06-01 11:24:09 +0200

    Feature: Complete CRUD on images

--- src/controller/product.controllers.ts
+++ src/controller/product.controllers.ts
@@ -6,7 +6,6 @@ import { ProductDoc } from "../dto/product.dto";
 
 export const addNew = asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
     const isTokenValid = await ValidateToken(req);
-    console.log(req.body);
     if (!isTokenValid) {
         return res.status(400).json({ message: "Access denied" });
     };
@@ -49,16 +48,7 @@ export const update = asyncWrapper(async (req: Request, res: Response, next: Nex
     if (!productToUpdate) {
         return res.status(404).json({ message: "Product not found" });
     }
-
-    if (req.files) {
-        req.body.imageFiles = [];
-        const files = req.files as [Express.Multer.File]
-        const images = files.map((file: Express.Multer.File) => file.filename);
-        images.forEach((image) => {
-            req.body.imageFiles.push(image);
-        });
-    };
-
+    
     // Save the updated product
     const updatedProduct = await ProductModel.findByIdAndUpdate(id, req.body, { new: true });
 
